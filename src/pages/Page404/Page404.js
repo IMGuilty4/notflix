d0727d40a1830252f8d708e76d4f32af94216c70
IMGuilty4 <61626947+IMGuilty4@users.noreply.github.com>
2021-12-05 04:36:34 +0400

    Add files via upload

--- src/pages/Page404/Page404.js
+++ src/pages/Page404/Page404.js
@@ -5,8 +5,8 @@ const Page404 = () => {
     return (
         <div className="page404__container">
             <h1>404</h1>
-            <h2>Page or movie no found</h2>
-            <Link to="/" className="button">Go Back to Home</Link>
+            <h2>Page or movie not found</h2>
+            <Link to="/" className="button">Go Back to Home Page</Link>
         </div>
     )
 }
